1652fd25894f99de0588249b7b6d856eb6dc3a9e
turbita2 <delacruzjose823@gmail.com>
2026-02-15 18:17:38 -0600

    update footer y navbar

--- Landing/js/main.js
+++ Landing/js/main.js
@@ -109,3 +109,14 @@
     
 })(jQuery);
 
+fetch("navbar.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+  });
+
+fetch("footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
